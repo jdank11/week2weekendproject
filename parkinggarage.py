49d1f3471cccbb2132bbbec938dee9b0cad5c567
Kevin <kgarcia0170@gmail.com>
2023-12-03 19:09:09 -0500

    Mypart

--- parkinggarage.py
+++ parkinggarage.py
@@ -9,15 +9,11 @@ class Parking():
     def payTicket(self):
         ticket = int(input('what is your ticket number? '))
         if self.currentTicket[ticket] == 'paid':
-            print('you have paid')
+            print('You have paid, Thank you have a nice day!')
         else:
             pay = input('would you like to pay? (Y/N))').upper()
             if pay.upper() == 'Y':
-                print('Thank you, please exit')
-                self.currentTicket[ticket] = 'paid'
-                self.parkingSpaces.append(ticket)
-                self.tickets.append(ticket)
-                print(f'your ticket status is: {self.currentTicket}')
+                self.leaveGarage(ticket)
             else:
                 print('please pay to exit')
                 self.payTicket()
@@ -30,11 +26,25 @@ class Parking():
             elif select == 'pay':
                 self.payTicket()
             elif select == 'exit':
-                self.leaveGarage()
+                self.leaveGarage("fromExit")
             elif select == 'quit':
                 break
             else:
                 print("would you like to park/pay/exit or quit ? ").lower()
+
+    def leaveGarage(self, ticket):
+        if ticket == "fromExit":
+            self.payTicket()
+        else:
+            print('Thank you, please exit')
+            self.currentTicket[ticket] = 'paid'
+            self.parkingSpaces.append(ticket)
+            self.tickets.append(ticket)
+            print("your ticket status is: ", self.currentTicket[ticket])
+            #print("Tickets = ", self.tickets)
+            #print("Parking Spaces = ", self.parkingSpaces)
+            #print("Current Ticket = ", self.currentTicket)
+
 
 car1 = Parking()
 car1.park()
